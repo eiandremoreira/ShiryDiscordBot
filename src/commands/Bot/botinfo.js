6cff63ecfe1e0c060ef5b339e5a1240b1849bb77
eiandremoreira <ninfetahot123@gmail.com>
2022-07-20 10:50:36 -0300

    Atualizando o comando botinfo para a versão nova do discord.js, confesso eu tinha esquecido

--- src/commands/Bot/botinfo.js
+++ src/commands/Bot/botinfo.js
@@ -36,11 +36,31 @@ module.exports = class Command {
 				.setTitle(idioma.botinfo.title)
 				.setColor('#6c92e4')
 				.setDescription(`> ${idioma.botinfo.description.replace(/{user}/g, interaction.user).replace(/{eu}/g, client.user.username).replace(/{total_servers}/g, client.guilds.cache.size).replace(/{cmds}/g, client.commands.size)}`)
-				.addField(`<:server:925053236862406696> ${idioma.botinfo.maquina}`, `> RAM: **${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB / ${Math.round((os.totalmem() / 1024 / 1024).toFixed(2) / 1024)} GB**\n> CPU: **${cpu}**\n> ${idioma.botinfo.processador}: **${os.cpus()[0].model}**`, false)
-				.addField(`<:add:925040703229268030> ${idioma.botinfo.meadd}`, `${idioma.botinfo.cliqueaqui}(https://discord.com/api/oauth2/authorize?client_id=850403894374694913&permissions=429966879974&scope=bot%20applications.commands) ${idioma.botinfo.toadd}`, true)
-				.addField(`<:upvote:925040657356161136> ${idioma.botinfo.vote}`, `${idioma.botinfo.cliqueaqui}(https://top.gg/bot/850403894374694913/vote) ${idioma.botinfo.tovote}`, true)
-				.addField(`<:way_correto:900128012517924914> ${idioma.botinfo.servidor}`, `${idioma.botinfo.cliqueaqui}(https://discord.com/invite) ${idioma.botinfo.log}`, true)
-				.addField(`🏆 ${idioma.botinfo.agradeco}`, `- ${idioma.botinfo.agradecimento.replace(/{user}/g, interaction.user).replace(/{andre}/g, andre.tag).replace(/{pessoas}/g, count).replace(/{total_servers}/g, client.guilds.cache.size)}`, false);
+				.addFields({
+					name: `<:server:925053236862406696> ${idioma.botinfo.maquina}`,
+					value: `> RAM: **${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB / ${Math.round((os.totalmem() / 1024 / 1024).toFixed(2) / 1024)} GB**\n> CPU: **${cpu}**\n> ${idioma.botinfo.processador}: **${os.cpus()[0].model}**`,
+					inline: false
+				},
+				{
+					name: `<:add:925040703229268030> ${idioma.botinfo.meadd}`,
+					value: `${idioma.botinfo.cliqueaqui}(https://discord.com/api/oauth2/authorize?client_id=850403894374694913&permissions=429966879974&scope=bot%20applications.commands) ${idioma.botinfo.toadd}`,
+					inline: true
+				},
+				{
+					name: `<:upvote:925040657356161136> ${idioma.botinfo.vote}`,
+					value: `${idioma.botinfo.cliqueaqui}(https://top.gg/bot/850403894374694913/vote) ${idioma.botinfo.tovote}`,
+					inline: true
+				},
+				{
+					name: `<:way_correto:900128012517924914> ${idioma.botinfo.servidor}`,
+					value: `${idioma.botinfo.cliqueaqui}(https://discord.com/invite) ${idioma.botinfo.log}`,
+					inline: true
+				},
+				{
+					name: `🏆 ${idioma.botinfo.agradeco}`,
+					value: `- ${idioma.botinfo.agradecimento.replace(/{user}/g, interaction.user).replace(/{andre}/g, andre.tag).replace(/{pessoas}/g, count).replace(/{total_servers}/g, client.guilds.cache.size)}`,
+					inline: false
+				});
 
 
 			interaction.reply({ embeds: [embed] });
